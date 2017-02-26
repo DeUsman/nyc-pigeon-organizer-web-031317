0fe795e9e2936ab608ddc55586fd5fd9162713de
Usman Pervaiz <de.usman@yahoo.com>
2017-02-26 05:37:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+new_object = {}
+
+ data.each do |key1, value1|
+   value1.each do |key2, value2|
+     value2.each do |name|
+       if(!new_object[name])
+       new_object[name] = {}
+       end 
+       if (!new_object[name][key1])
+       new_object[name][key1] = []
+     end
+       new_object[name][key1].push(key2.to_s)
+     end
+    end
+  end
+  new_object
 end
