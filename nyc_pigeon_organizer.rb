b4567e1224f5a214b54d8270fccb8c348121c64e
Usman Pervaiz <de.usman@yahoo.com>
2017-02-26 06:09:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,10 +6,10 @@ new_object = {}
      value2.each do |name|
        if(!new_object[name])
        new_object[name] = {}
-       end 
-       if (!new_object[name][key1])
+       end
+       if(!new_object[name][key1])
        new_object[name][key1] = []
-     end
+       end
        new_object[name][key1].push(key2.to_s)
      end
     end
